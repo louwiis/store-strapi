b5eeb27f1c4c10f50ae6ea3418d45b90088e4d9e
Luis Teixeira Lima <louwis@MacBook-Pro-de-Luis.local>
2024-03-24 15:22:36 +0100

    Exercice 3: Modif config strapi findOne  slug

--- src/api/product/controllers/product.ts
+++ src/api/product/controllers/product.ts
@@ -4,4 +4,26 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::product.product');
+export default factories.createCoreController('api::product.product', ({ strapi }) => ({
+    async findOne(ctx) {
+        const { id } = ctx.params
+        console.log('slug', ctx.params)
+        const entity = await strapi.db.query('api::product.product').findOne({
+            where: {
+                slug: id
+            },
+            populate: {
+                'image': {
+                    select: ['url', 'alternativeText']
+                },
+                'category': {
+                    select: ['name', 'slug']
+                }
+            }
+        })
+
+        const sanitizedEntity = await this.sanitizeOutput(entity, ctx)
+
+        return this.transformResponse(sanitizedEntity)
+    },
+}))
