0daa0496bc7f597243751142becbde7b63761198
Luis Teixeira Lima <louwis@MacBook-Pro-de-Luis.local>
2024-03-25 22:29:01 +0100

    Exercice 5: Remove personnal config on categories

--- src/api/category/controllers/category.ts
+++ src/api/category/controllers/category.ts
@@ -4,28 +4,4 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::category.category', ({ strapi }) => ({
-    async findOne(ctx) {
-        const { id } = ctx.params
-        console.log('slug', ctx.params)
-        const entity = await strapi.db.query('api::category.category').findOne({
-            where: {
-                slug: id
-            },
-            populate: {
-                'products': {
-                    select: ['name', 'slug', 'price'],
-                    populate: {
-                        'image': {
-                            select: ['url', 'alternativeText']
-                        }
-                    }
-                }
-            }
-        })
-
-        const sanitizedEntity = await this.sanitizeOutput(entity, ctx)
-
-        return this.transformResponse(sanitizedEntity)
-    },
-}))
+export default factories.createCoreController('api::category.category');
